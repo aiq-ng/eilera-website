40a4ae9469a0edffe58bb01fd2fcf30a93ac2bc5
Gerald Ezenagu <egerald344@gmail.com>
2024-03-13 21:22:41 +0100

    Landing screen

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  constructor(private renderer: Renderer2) {}
   navItems = [
-    { path: "#privacy", label: "Privacy" },
-    { path: "#help", label: "Help Center" },
-    { path: "#blog", label: "Blog" },
-    { path: "#features", label: "Features" },
-    
+    { path: '#privacy', label: 'Privacy' },
+    { path: '#help', label: 'Help Center' },
+    { path: '#blog', label: 'Blog' },
+    { path: '#features', label: 'Features' },
   ];
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const sectionHead = document.querySelector('.section-head');
+    const navControl = document.querySelector('.nav-control');
+
+    if (window.scrollY > 0) {
+      this.renderer.addClass(sectionHead, 'scrolled');
+      this.renderer.addClass(navControl, 'scrolled');
+    } else {
+      this.renderer.removeClass(sectionHead, 'scrolled');
+      this.renderer.removeClass(navControl, 'scrolled');
+    }
+  }
 }
